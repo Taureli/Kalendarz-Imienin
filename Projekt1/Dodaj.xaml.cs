8498b64518b7a64c139fc3972295021d67578f85
Jakub Karolczak <hobkinss@gmail.com>
2014-03-17 12:05:42 +0100

    Pierwszy commit
    -Integracja z bazą danych
    -Wyświetlanie aktualnej daty
    -Wyświetlanie dzisiejszych i jutrzejszych imienin
    -Oznaczanie osób jako znajomi + specjalne oznaczenie, gdy mają imieniny
    -Osobne strony dla dodawania i usuwania znajomych
    
    TO DO:
    *Usuwanie znajomych
    *Poprawienie wyznaczania daty następnego dnia
    *Zamiana sposobu dodawania nowych znajomych na wyszukiwanie
    *Dodanie strony 'o programie'
    *Poprawki wizualne

---
+++ Projekt1/Dodaj.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Projekt1
+{
+    public partial class Dodaj : PhoneApplicationPage
+    {
+        public Dodaj()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            using (ImieninyContext ctx = new ImieninyContext(ImieninyContext.ConnectionString))
+            {
+                ctx.CreateIfNotExists();
+                ctx.LogDebug = true;
+                // MainLongListSelector.ItemsSource = ctx.Tabela.ToList();
+                DodajList.ItemsSource = ctx.Tabela.Where(d => d.Znajomy == "Collapsed").OrderBy(d => d.Imieniny).ToList();
+
+            }
+        }
+
+        private void DodajButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (ImieninyContext ctx = new ImieninyContext(ImieninyContext.ConnectionString))
+            {
+                ctx.LogDebug = true;
+                Button _button = (Button)sender;
+                //var osoba = ctx.Tabela.Where(d => d.Id == (int)_button.Tag).Single();
+
+                var bla = (from p in ctx.Tabela where p.Id == (int)_button.Tag select p).Single();
+                bla.Znajomy = "Visible";
+
+                //osoba.Znajomy = "Visible";
+
+                ctx.SubmitChanges();
+            }
+        }
+
+    }
+}
---
+++ Projekt1/MainPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Projekt1.Resources;
+
+namespace Projekt1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DayText.Text = DateTime.Today.Day.ToString();
+            showMonth();
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            using (ImieninyContext ctx = new ImieninyContext(ImieninyContext.ConnectionString))
+            {
+                ctx.CreateIfNotExists();
+                ctx.LogDebug = true;
+               // MainLongListSelector.ItemsSource = ctx.Tabela.ToList();
+                MainLongListSelector.ItemsSource = ctx.Tabela.Where(d => d.Dzien == DateTime.Today.Day).Where(d => d.Miesiac == DateTime.Today.Month).ToList();
+
+
+                JutroList.ItemsSource = ctx.Tabela.Where(d => d.Dzien == DateTime.Today.Day + 1).Where(d => d.Miesiac == DateTime.Today.Month).ToList();
+
+                ZnajomiList.ItemsSource = ctx.Tabela.Where(d => d.Znajomy == "Visible").ToList();
+
+            }
+        }
+
+        void showMonth(){
+
+            int month = DateTime.Today.Month;
+
+            switch (month)
+            {
+
+                case(1):
+                    MonthText.Text = "Stycznia";
+                    break;
+                case (2):
+                    MonthText.Text = "Lutego";
+                    break;
+                case (3):
+                    MonthText.Text = "Marca";
+                    break;
+                case (4):
+                    MonthText.Text = "Kwietnia";
+                    break;
+                case (5):
+                    MonthText.Text = "Maja";
+                    break;
+                case (6):
+                    MonthText.Text = "Czerwca";
+                    break;
+                case (7):
+                    MonthText.Text = "Lipca";
+                    break;
+                case (8):
+                    MonthText.Text = "Sierpnia";
+                    break;
+                case (9):
+                    MonthText.Text = "Września";
+                    break;
+                case (10):
+                    MonthText.Text = "Października";
+                    break;
+                case (11):
+                    MonthText.Text = "Listopada";
+                    break;
+                case (12):
+                    MonthText.Text = "Grudnia";
+                    break;
+
+            }
+
+        }
+
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Dodaj.xaml", UriKind.Relative));
+        }
+    }
+}
